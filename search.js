43f99a825edb896afc972c18a57c88f919c91e95
Lucescu-Andrei <167630596+Lucescu-Andrei@users.noreply.github.com>
2024-05-09 00:55:54 +0300

    Update search.js

--- search.js
+++ search.js
@@ -27,4 +27,35 @@ let sugerimente=[
     'bere',
     'pateu de porc',
     'cartofi',
-]
+];
+const sugestie = document.querySelector(".sugestie");
+const caut = document.getElementById("search");
+
+caut.onkeyup = function(){
+    let s =[];
+    let src = caut.value;
+    if(src.length){
+        s = availableKeyword.filter((keyword)=>{
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(s);
+    }
+    display(s);
+
+    if(!s.length){
+        sugestie.innerHTML='';
+    }
+}
+
+function display(s){
+    const content =  s.map((list)=>{
+        return"<li onclick=selectInput(this)>" + list + "</li>";
+    });
+
+    sugestie.innerHTML ="<ul>" + content.join('') + "</ul>";
+}
+
+function selectInput(list){
+    caut.value = list.innerHTML;
+    sugestie.innerHTML = '';
+}
